dabe8428a66dfbfa7acb3ca47fd51387f6098887
Alan Singfield <alan.singfield.work@ntlworld.com>
2020-02-28 17:31:05 +0000

    PathRedux first trial version

---
+++ PathReduxTests/PathRedux/PathStorageTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using YellowCounter.FileSystemState.PathRedux;
+using Shouldly;
+
+namespace YellowCounter.FileSystemState.Tests.PathRedux
+{
+    [TestClass]
+    public class PathStorageTests
+    {
+        [TestMethod]
+        public void PathStorage1()
+        {
+            var pathStorage = new PathStorage();
+
+            int idx1 = pathStorage.Store("Hello");
+            int idx2 = pathStorage.Store("World");
+
+            pathStorage.Retrieve(idx1).ToString().ShouldBe("Hello");
+            pathStorage.Retrieve(idx2).ToString().ShouldBe("World");
+        }
+
+        [TestMethod]
+        public void PathStorage2()
+        {
+            var pathStorage = new PathStorage();
+
+            int idx1 = pathStorage.Store("Hello");
+            int idx2 = pathStorage.Store("World");
+
+            pathStorage.Retrieve(new[] { idx1, idx2 }).ToString().ShouldBe("HelloWorld");
+        }
+    }
+}
